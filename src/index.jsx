5f5d3291b68dd76250a3b371ed9e2ae1291d4ecd
Josh <aresx130@gmail.com>
2018-02-16 16:32:16 -0800

    Fix linting errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -13,4 +14,5 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'),
+);
